ce90609a901c766482c9a7ee006d5934116cf9d7
olivier <olivier@olivier.(none)>
2011-09-03 22:50:18 +0200

    add rails project

---
+++ cassiopee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cassiopee::Application.config.secret_token = '7989853ff043439f65f26dbae6aa094e0a606ceb1df15c35bb4e0cbcc47146dd7de1ee75bb832dff50e95f245ef407b15571f7e26322fed8093f1ffa3830167b'
