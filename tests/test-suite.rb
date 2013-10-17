7fb169cb78e8661c94471542324c7e9243bbb901
Olivier Sallou <olivier.sallou@irisa.fr>
2013-10-17 13:15:28 +0200

    remove debug logs

--- tests/test-suite.rb
+++ tests/test-suite.rb
@@ -22,16 +22,9 @@ class TestCrawler < Test::Unit::TestCase
 
   def test_exactsearch2
     crawler = Cassiopee::Crawler.new
-    crawler.setLogLevel(Logger::DEBUG)
     crawler.indexString('my sample example')
     matches = crawler.searchExact('xample')
     assert_equal(1,matches.length)
-    # Minus 1, because first element is len of match
-    match = crawler.next()
-    puts "## "+match.to_s
-    #assert_equal(2,match[2].length-1)
-    crawler.setLogLevel(Logger::ERROR)
-
   end
 
 
